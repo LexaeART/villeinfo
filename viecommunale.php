9c27056416fd532796e51ae52f768187d29f6148
LexaeART <astheae@gmail.com>
2019-06-14 11:59:36 +0200

    Vie communale

---
+++ viecommunale.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <!--        <base href="https://www.villeinfo.fr" />-->
+  <!--        <meta name="generator" content="Jekyll v3.8.5" />-->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="Kaiartsu est une agence web basée en Hauts-de-France. Spécialisée dans le développement de site web et d'identité visuelle." />
+  <meta name="publisher" content="Kaiartsu">
+  <meta name="author" content="Kaiartsu" />
+  <meta name="reply-to" content="contact@kaiartsu.fr">
+  <title>Accueil | Vie communale | Mairie de la commune de Ville | Hauts-de-France</title>
+  <!-- Link CSS -->
+  <link rel="shortcut icon" type="assets/images/logo/fav.png" href="assets/images/logo/fav.png"/>
+  <link href="assets/lib/bootstrap/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/viecommunale.css" rel="stylesheet" type="text/css"/>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Associations</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Activités</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Ecole</a>
+        </li>
+      </ul>
+        <a class="navbar-brand" href="#"><img src="assets/images/logoville.png" /></a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Patrimoine</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Démarches administratives</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <main id="">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12" id="VIN">
+          <div class="col-1" id="important">
+            <p>
+              A la une
+            </p>
+            <hr />
+          </div>
+          <div class="col-5" id="title">
+            <p>
+              Titre de l'article
+            </p>
+          </div>
+          <div class="col-2" id="read">
+            <p>
+              Lire l'article
+            </p>
+            <hr />
+          </div>
+        </div>
+      </div>
+      <div class="row" id="actualités">
+        <div class="offset-1 col-4">
+          <hr />
+        </div>
+        <div class=" col-2" id="actu">
+          <h1>Actualités</h1>
+        </div>
+        <div class=" col-4">
+          <hr />
+        </div>
+      </div>
+    </div>
+  </main>
+  <script src="assets/lib/bootstrap/js/bootstrap.js" type="text/javascript"></script>
+    <script src="assets/js/viecommunale.js" type="text/javascript"></script>
+  </body>
+  </html>
