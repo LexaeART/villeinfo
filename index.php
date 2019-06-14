64218bebe76fb3b8edb83ff95a1db5b48d9f11d3
LexaeART <astheae@gmail.com>
2019-06-14 09:23:58 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <!--        <base href="https://www.villeinfo.fr" />-->
+  <!--        <meta name="generator" content="Jekyll v3.8.5" />-->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="Kaiartsu est une agence web basée en Hauts-de-France. Spécialisée dans le développement de site web et d'identité visuelle." />
+  <meta name="publisher" content="Kaiartsu">
+  <meta name="author" content="Kaiartsu" />
+  <meta name="reply-to" content="contact@kaiartsu.fr">
+  <title>Accueil | Mairie de la commune de Ville | Hauts-de-France</title>
+  <!-- Link CSS -->
+  <link rel="shortcut icon" type="assets/images/logo/fav.png" href="assets/images/logo/fav.png"/>
+  <link href="assets/lib/bootstrap/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/main.css" rel="stylesheet" type="text/css"/>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+
+    <script src="assets/js/main.js" type="text/javascript"></script>
+  </body>
+  </html>
