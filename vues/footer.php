e6475f41d18ab4f4ce42538afb7deb4c2f3ff566
LexaeART <astheae@gmail.com>
2019-07-18 15:39:33 +0200

    Footer

--- vues/footer.php
+++ vues/footer.php
@@ -1,3 +1,42 @@
 <footer>
-  
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h3>Restez connecté</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs12 col-sm-12 col-md-6 col-lg-4">
+        <h4>Newsletter</h4>
+        <hr />
+        <p>
+          Chaque mois, recevez l’essentiel de Ville pour savoir tout ce qu’il se passe dans la commune.
+        </p>
+        <button class="fill" onclick="window.location.href='index.php'">Je m'abonne</button>
+      </div>
+      <div class="col-xs12 col-sm-12 col-md-6 col-lg-4">
+        <h4>Contactez Ville</h4>
+        <hr />
+        <p>
+          La Mairie de Ville répond à toutes vos questions. A votre service du lundi au vendredi.
+        </p>
+        <button class="fill" onclick="window.location.href='mailto:san@antonio.net'">Contact</button>
+      </div>
+      <div class="col-xs12 col-sm-12 col-md-6 col-lg-4">
+        <h4>Liens pratiques</h4>
+        <hr />
+        <ul>
+          <li>
+            <a href="">Mentions Légales</a>
+          </li>
+          <li>
+            <a href="">Charte de confidentialité</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </footer>
